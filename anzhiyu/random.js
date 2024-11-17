6403c0c4573ea0a6674ed05f3d4cafb32bf6218c
coder_alex <ne_lucifer@163.com>
2024-11-17 11:28:30 +0800

    Site updated: 2024-11-17 11:28:07

---
+++ anzhiyu/random.js
@@ -0,0 +1,56 @@
+var posts=["2024_bf/工具/Hexo-搭建个人博客1.html","2024_bf/工具/Hexo-搭建个人博客-报错集锦.html","2024_bf/工具/Hexo-搭建个人博客2.html","2024_bf/工具/Jrebel激活破解方式（正在使用）.html","2024_bf/工具/Hexo-搭建个人博客3.html","2024_bf/工具/工具使用-Git-基本使用.html","2024_bf/工具/工具使用-GitBook-使用.html","2024_bf/工具/工具使用-Idea-常用插件.html","2024_bf/工具/工具使用-Vmware-问题1.html","2024_bf/工具/工具使用-Idea-常见问题.html","2024_bf/其他/WeChat-login.html","2024_bf/前端/Npm-常用命令.html","2024_bf/读书笔记/读书笔记-陀氏-穷人的美德.html","2024_bf/前端/Creat-react-app-使用笔记-部署方式.html","2024_bf/前端/Freemarker-前端模板引擎使用.html","2024_bf/前端/Hexo.html","2024_bf/前端/Bootstrap/Bootstrap-分享笔记1.html","2024_bf/前端/Bootstrap/Bootstrap-插件-bootstrap-table-export.html","2024_bf/前端/JQuery/JQuery-学习笔记0-样式篇.html","2024_bf/前端/JQuery/JQuery-学习笔记2-DOM篇.html","2024_bf/前端/JQuery/JQuery-学习笔记1-样式篇.html","2024_bf/前端/JQuery/JQuery-学习笔记3-DOM篇.html","2024_bf/前端/JavaScript/JavaScript-array.html","2024_bf/前端/Vue/Vue-学习笔记0-独立项目搭建.html","2024_bf/前端/JavaScript/JavaScript-utils.html","2024_bf/前端/JavaScript/JavaScript-防止js全局变量污染方法总结.html","2024_bf/服务端/Java/Java8.html","2024_bf/前端/Vue/Vue-学习笔记1-Vue常用方法.html","2024_bf/服务端/Java/Java并发编程.html","2024_bf/服务端/Java/Java基础知识.html","2024_bf/服务端/Java/Quartz-学习笔记-相关概念.html","2024_bf/前端/Vue/Vue-学习笔记2-Vue登录实例.html","2024_bf/服务端/Java/Java系列-SpringBoot-1-入参及出参扩展XML支持.html","2024_bf/服务端/Java/Quartz-学习笔记1-小试牛刀.html","2024_bf/服务端/Java/Quartz-学习笔记2-界面配置.html","2024_bf/服务端/Java/Java系列-SpringBoot学习笔记1.html","2024_bf/服务端/Java/README.html","2024_bf/服务端/Java/SSM框架-整合笔记.html","2024_bf/服务端/Java/SSM报错处理.html","2024_bf/服务端/Java/SSM框架-整合笔记0-详细注释.html","2024_bf/服务端/Java/SSM框架-整合笔记1-Druid和Webservice配置.html","2024_bf/服务端/Java/SSM框架-整合笔记11-配置Redis.html","2024_bf/服务端/Java/SSM框架-整合笔记2-视图解析器配置.html","2024_bf/服务端/Java/SSM框架-整合笔记10-配置工作流Activiti.html","2024_bf/服务端/Java/SSM框架-整合笔记4-统一异常处理.html","2024_bf/服务端/Java/SSM框架-整合笔记3-使用注解配置切面.html","2024_bf/服务端/Java/SSM框架-整合笔记5-缓存配置Ehcache.html","2024_bf/服务端/Java/SSM框架-整合笔记9-注解方式配置Quartz定时器.html","2024_bf/服务端/Java/SSM框架-整合笔记7-配置Swagger.html","2024_bf/服务端/Java/SSM框架-整合笔记6-配置Mybatis Generator.html","2024_bf/服务端/Java/SSM框架-整合笔记8-Shiro介绍.html","2024_bf/服务端/Java/Spring学习笔记2-Spring核心组件.html","2024_bf/服务端/Java/Spring学习笔记1-Ioc与AOP.html","2024_bf/服务端/Java/Shiro系列-学习笔记0-目录.html","2024_bf/服务端/Java/数据结构.html","2024_bf/服务端/Java/Shiro系列-学习笔记1-基本概念.html","2024_bf/服务端/Java/Shiro系列-常见问题处理.html","2024_bf/服务端/Python/Python-学习笔记0-环境变量及Pycharm配置.html","2024_bf/服务端/常用工具/Excel解析-POI解析实例.html","2024_bf/服务端/常用工具/SoapUI-webservice.html","2024_bf/服务端/常用工具/SoapUI-调试webservice接口.html","2024_bf/服务端/数据库/MongoDB系列-1-查询.html","2024_bf/服务端/数据库/Mysql-export-database.html","2024_bf/服务端/数据库/MongoDB系列-0-安装.html","2024_bf/服务端/数据库/Mysql数据库常用方式.html","2024_bf/服务端/数据库/Oracle中的常用函数.html","2024_bf/服务端/数据库/PowerDesigner-export-PDM-rtf.html","2024_bf/服务端/框架/Mybatis常见错误记录.html","2024_bf/服务端/框架/RBAC权限管理系统设计.html","2024_bf/服务端/数据库/Redis系列-0-Windows下安装Redis服务.html","2024_bf/服务端/数据库/Sqlserver2008r2.html","2024_bf/服务端/框架/Springmvc使用常见问题.html","2024_bf/服务端/运维/CentOS7搭建Jenkins.html","2024_bf/服务端/运维/CentOS7搭建Nexus-Maven私人仓库.html","2024_bf/服务端/运维/Linux使用1_1—CentOS7离线安装MySQL服务.html","2024_bf/服务端/运维/Linux使用1—CentOS7初始化配置.html","2024_bf/服务端/运维/Linux使用3—CentOS7安装MongoDB数据库.html","2024_bf/服务端/运维/Linux使用2—CentOS7安装redis服务.html","2024_bf/服务端/运维/Linux使用4—CentOS7安装Docker及基本使用.html","2024_bf/服务端/运维/Windows-server-2012.html","2024_bf/服务端/运维/Linux使用6—CentOS7运维常用命令.html","2024_bf/服务端/运维/Linux使用5—CentOS7安装ftp.html","2024_bf/服务端/运维/Nginx反向代理和负载均衡使用.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg","color":"vip","tag":"技术"},{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","recommend":true}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
